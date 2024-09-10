da04794a4f3eedc2ff7e181604b82711fc66760f
KaitlynClay <kotlin9860@gmail.com>
2024-09-10 10:00:16 -0500

    Initial Commit

---
+++ linking3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  linking3
+//
+//  Created by student on 9/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, World!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ linking3/linking3App.swift
@@ -0,0 +1,17 @@
+//
+//  linking3App.swift
+//  linking3
+//
+//  Created by student on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct linking3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
